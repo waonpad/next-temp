29aede66f8b732ee8b3d91c628fbe9b8cee7783b
waonpad <waonpad@gmail.com>
2024-09-22 13:44:47 +0900

    nextcanary

--- src/config/url/host-api.ts
+++ src/config/url/host-api.ts
@@ -1,4 +1,4 @@
-import { clientEnv } from "@/config/env";
+import { clientEnv } from "@/config/env/client";
 import type { NoLeadingSlash } from "@/types";
 
 export const hostApi = <T extends string>(path: NoLeadingSlash<T>) => {
